94e96338157cf13a001c280068958d2f134d6882
Lại Trung Luân <v.luanlt6@vinid.net>
2020-02-03 10:28:36 +0700

    RIBs

---
+++ RIB Unit Tests.xctemplate/___FILEBASENAME___InteractorTests.swift
@@ -0,0 +1,11 @@
+//___FILEHEADER___
+
+@testable import ___PROJECTNAME___
+import XCTest
+
+final class ___VARIABLE_productName___InteractorTests: XCTestCase {
+
+    private var interactor: ___VARIABLE_productName___Interactor!
+    
+    func test_exampleObservable_callsRouterOrListener_exampleProtocol() {}
+}
---
+++ RIB Unit Tests.xctemplate/___FILEBASENAME___RouterTests.swift
@@ -0,0 +1,11 @@
+//___FILEHEADER___
+
+@testable import ___PROJECTNAME___
+import XCTest
+
+final class ___VARIABLE_productName___RouterTests: XCTestCase {
+
+    private var router: ___VARIABLE_productName___Router!
+    
+    func test_routeToExample_invokesToExampleResult() {}
+}
